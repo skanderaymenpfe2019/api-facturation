ae2bffa0420b8a5e8c9f0f8034d726f2e07d91cd
Skander Samoud <ssamoud76@gmail.com>
2019-05-07 01:32:58 +0100

    update DevisController, Ajout dans Route API

--- app/Http/Controllers/DevisController.php
+++ app/Http/Controllers/DevisController.php
@@ -24,29 +24,29 @@ class DevisController extends Controller
     public function updateDevis(Request $request, $id)
     {
         $quotations = DB:: table('quotations')->where('id',$request->input('id'))->get();
-        $quotations->client_id = $request->input('client_id');
-        $quotations->ref = $request->input('ref');
-        $quotations->regulation = $request->input('regulation');
-        $quotations->remark = $request->input('remark');
-        $quotations->date = $request->input('date');
-        $quotations->date_deadline = $request->input('date_deadline');
-        $quotations->ref_client = $request->input('ref_client');
-        $quotations->name = $request->input('name');
-        $quotations->email = $request->input('email');
-        $quotations->adress = $request->input('adress');
-        $quotations->postcode = $request->input('postcode');
-        $quotations->city = $request->input('city');
-        $quotations->tva_client = $request->input('tva_client');
-        $quotations->country_id = $request->input('country_id');
-        $quotations->phone = $request->input('phone');
-        $quotations->fax = $request->input('fax');
-        $quotations->payment_mdoe = $request->input('payment_mode');
-        $quotations->total_price = $request->input('total_price');
-        $quotations->advance = $request->input('advance');
-        $quotations->invoiced = $request->input('invoiced');
-        $quotations->status = $request->input('status');
-        $quotations->currency_id = $request->input('currency_id');
-        $quotations->save();
+        $quotation->client_id = $request->input('client_id');
+        $quotation->ref = $request->input('ref');
+        $quotation->regulation = $request->input('regulation');
+        $quotation->remark = $request->input('remark');
+        $quotation->date = $request->input('date');
+        $quotation->date_deadline = $request->input('date_deadline');
+        $quotation->ref_client = $request->input('ref_client');
+        $quotation->name = $request->input('name');
+        $quotation->email = $request->input('email');
+        $quotation->adress = $request->input('adress');
+        $quotation->postcode = $request->input('postcode');
+        $quotation->city = $request->input('city');
+        $quotation->tva_client = $request->input('tva_client');
+        $quotation->country_id = $request->input('country_id');
+        $quotation->phone = $request->input('phone');
+        $quotation->fax = $request->input('fax');
+        $quotation->payment_mdoe = $request->input('payment_mode');
+        $quotation->total_price = $request->input('total_price');
+        $quotation->advance = $request->input('advance');
+        $quotation->invoiced = $request->input('invoiced');
+        $quotation->status = $request->input('status');
+        $quotation->currency_id = $request->input('currency_id');
+        $quotation->save();
            $response["quotations"] = $quotations;
            $response["success"] = 1;
         return response()->json($response);
@@ -55,8 +55,8 @@ class DevisController extends Controller
     // Supprimer Devis
 
     public function deleteDevis($id){
-        $quotations  = DB::table('quotations')->where('id',$request->input('id'))->get();
-        $quotations->delete();
+        $quotation  = DB::table('quotations')->where('id',$request->input('id'))->get();
+        $quotation->delete();
         return response()->json('Removed successfully.');
     }
 
@@ -70,7 +70,7 @@ class DevisController extends Controller
     }
 
     // Aeprcue en PDF
-    
+
     public function generatePDF(){
         $data = ['title' =>'Cher Client, Veuillez trouver en annexe votre devis , tout en sachant que nous y avons intégrée les meilleurs conditions tarifaire , ajustée a votre demande de renseignement , au plaisir de vous conduire , bien à vous merci .'];
         $pdf = PDF::loadView('myPDF', $data);
--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 });
 
+// Route Client :
+
 Route::post('clients', 'ClientController@createClient');
 Route::put('client/{id}','ClientController@updateClient');
 Route::delete('client/{id}','ClientController@deleteClient');
 Route::get('clients','ClientController@index');
+
+// Route Commande :
 
 Route::post('commands', 'CommandeController@createCommande');
 Route::put('command/{id}','CommandeController@updateCommande');
@@ -31,10 +35,21 @@ Route::get('commands','CommandeController@generatePDF');
 Route::post('commands','CommandeController@attachmentEMAIL');
 Route::post('commands','CommandeController@createFacture');
 
+// Route Facture :
+
 Route::post('factures','FactureController@createFacture');
 Route::put('facture/{id}','FactureController@updateFacture');
 Route::delete('facture/{id}','FactureController@deleteFacture');
 Route::get('factures','FactureController@idex');
-Route::get('facture','CommandeController@generatePDF');
+Route::get('facture','FactureController@generatePDF');
 Route::post('factures','FactureController@attachmentEMAIL');
-Route::post('factures','FactureController@createNoteCredit');
+Route::post('facture','FactureController@createNoteCredit');
+
+// Route Devis :
+
+Route::post('quotations','DevisController@createDevis');
+Route::put('quotation/{id}','DevisController@updateDevis');
+Route::delete('quotation/{id}','DevisController@deleteDevis');
+Route::get('quotations','DevisController@index');
+Route::get('quotation','DevisController@generatePDF');
+Route::post('quotation','DevisController@createCommande');
